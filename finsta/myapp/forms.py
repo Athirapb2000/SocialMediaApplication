de0448bfb7f30c8d136a14eaa6a740f65606b568
athira <athirapb210920@gmai.com>
2023-04-11 11:32:36 +0530

    changes

--- finsta/myapp/forms.py
+++ finsta/myapp/forms.py
@@ -6,3 +6,7 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model=User
         fields=["first_name","last_name","email","username","password1","password2"]
+
+class SignInForm():
+    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
